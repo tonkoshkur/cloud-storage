399831f2a54c38638515ea7463ed2448d17ba4cb
Ilya Tonkoshkur <54292571+tonkoshkur@users.noreply.github.com>
2024-07-11 10:29:06 +0200

    feat(signup): add validation messages to SignUpRequest fields

--- src/main/java/ua/tonkoshkur/cloudstorage/auth/signup/SignUpRequest.java
+++ src/main/java/ua/tonkoshkur/cloudstorage/auth/signup/SignUpRequest.java
@@ -5,10 +5,10 @@ import jakarta.validation.constraints.Size;
 @PasswordMatches
 public record SignUpRequest(
 
-        @Size(min = 3)
+        @Size(min = 3, message = "Username must be 3+ characters")
         String username,
 
-        @Size(min = 3)
+        @Size(min = 3, message = "Password must be 3+ characters")
         String password,
 
         String confirmPassword
