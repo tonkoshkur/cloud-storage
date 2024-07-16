5d745bceb95ea4e86719c467ddbba271026a264a
Ilya Tonkoshkur <54292571+tonkoshkur@users.noreply.github.com>
2024-07-16 16:05:08 +0200

    refactor(signup): delegate request validation to server-side

--- src/main/java/ua/tonkoshkur/cloudstorage/auth/signup/SignUpRequest.java
+++ src/main/java/ua/tonkoshkur/cloudstorage/auth/signup/SignUpRequest.java
@@ -5,10 +5,14 @@ import jakarta.validation.constraints.Size;
 @PasswordMatches
 public record SignUpRequest(
 
-        @Size(min = 3, message = "Username must be 3+ characters")
+        @Size(min = 3,
+                max = 50,
+                message = "Username must be between 3 and 50 characters")
         String username,
 
-        @Size(min = 3, message = "Password must be 3+ characters")
+        @Size(min = 3,
+                max = 50,
+                message = "Password must between 3 and 50 characters")
         String password,
 
         String confirmPassword
