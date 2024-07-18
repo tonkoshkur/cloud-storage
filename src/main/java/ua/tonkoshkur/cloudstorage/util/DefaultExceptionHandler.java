ae601449241994edc3284263ded4a567e99c9601
Ilya Tonkoshkur <54292571+tonkoshkur@users.noreply.github.com>
2024-07-18 15:10:16 +0200

    fix(file): set max size and handle MaxUploadSizeExceededException

--- src/main/java/ua/tonkoshkur/cloudstorage/util/DefaultExceptionHandler.java
+++ src/main/java/ua/tonkoshkur/cloudstorage/util/DefaultExceptionHandler.java
@@ -1,8 +1,10 @@
 package ua.tonkoshkur.cloudstorage.util;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import ua.tonkoshkur.cloudstorage.file.FileAlreadyExistsException;
 import ua.tonkoshkur.cloudstorage.file.InvalidFileNameException;
 import ua.tonkoshkur.cloudstorage.folder.FolderAlreadyExistsException;
@@ -11,6 +13,9 @@ import ua.tonkoshkur.cloudstorage.folder.InvalidFolderNameException;
 @ControllerAdvice
 public class DefaultExceptionHandler {
 
+    @Value("${spring.servlet.multipart.max-file-size}")
+    private String maxFileSize;
+
     @ExceptionHandler({
             FileAlreadyExistsException.class,
             FolderAlreadyExistsException.class,
@@ -18,5 +23,11 @@ public class DefaultExceptionHandler {
             InvalidFolderNameException.class})
     public String handleCustomException(Exception e, HttpServletRequest request) {
         return UrlHelper.buildRefererRedirectUrlWithParam(request, "error", e.getMessage());
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public String handleMaxUploadSizeExceededException(HttpServletRequest request) {
+        return UrlHelper.buildRefererRedirectUrlWithParam(request, "error",
+                "File size must less than " + maxFileSize);
     }
 }
