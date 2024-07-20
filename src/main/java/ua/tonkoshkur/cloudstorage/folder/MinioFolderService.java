f5498a49ca212bfb58c948961c3668523141ec3a
Ilya Tonkoshkur <54292571+tonkoshkur@users.noreply.github.com>
2024-07-20 21:36:18 +0200

    refactor(folder): enhance minio result items mapping code clarity and maintainability

--- src/main/java/ua/tonkoshkur/cloudstorage/folder/MinioFolderService.java
+++ src/main/java/ua/tonkoshkur/cloudstorage/folder/MinioFolderService.java
@@ -32,7 +32,10 @@ public class MinioFolderService implements FolderService {
     public List<FolderDto> findAllByQuery(long userId, String query) {
         String userFolderPath = getUserFolderPath(userId);
         Iterable<Result<Item>> results = minioService.findAll(userFolderPath, true);
-        return resultItemsMapper.map(results, userFolderPath, query);
+        return resultItemsMapper.map(results, userFolderPath)
+                .stream()
+                .filter(folder -> StringUtils.containsIgnoreCase(folder.name(), query))
+                .toList();
     }
 
     @SneakyThrows
@@ -45,7 +48,10 @@ public class MinioFolderService implements FolderService {
 
         Iterable<Result<Item>> results = minioService.findAll(prefix, false);
 
-        return resultItemsMapper.map(results, userFolderPath, null);
+        return resultItemsMapper.map(results, userFolderPath)
+                .stream()
+                .filter(folder -> !folder.path().equals(parentFolderPath))
+                .toList();
     }
 
     @SneakyThrows
--- src/main/java/ua/tonkoshkur/cloudstorage/folder/MinioResultItemsToFolderDtoMapper.java
+++ src/main/java/ua/tonkoshkur/cloudstorage/folder/MinioResultItemsToFolderDtoMapper.java
@@ -6,7 +6,6 @@ import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
 import ua.tonkoshkur.cloudstorage.util.PathHelper;
 
-import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,30 +15,29 @@ import static ua.tonkoshkur.cloudstorage.util.PathHelper.PATH_SEPARATOR;
 public class MinioResultItemsToFolderDtoMapper {
 
     @SneakyThrows
-    public List<FolderDto> map(Iterable<Result<Item>> results, String userFolderPath, @Nullable String query) {
+    public List<FolderDto> map(Iterable<Result<Item>> results, String userFolderPath) {
         List<FolderDto> folders = new ArrayList<>();
         for (Result<Item> result : results) {
             Item item = result.get();
-            if (shouldSkipResultItem(item, userFolderPath, query)) {
+            String fullPath = item.objectName();
+            if (isFile(fullPath) || userFolderPath.equals(fullPath)) {
                 continue;
             }
-            String path = getShortPath(userFolderPath, item.objectName());
-            folders.add(new FolderDto(
-                    PathHelper.extractName(path),
-                    PathHelper.extractParentFolder(path)));
+            FolderDto folder = mapItem(item, userFolderPath);
+            folders.add(folder);
         }
         return folders;
     }
 
-    private boolean shouldSkipResultItem(Item item, String userFolderPath, @Nullable String query) {
-        if (query == null && !item.isDir()) {
-            return true;
-        }
-        String fullPath = item.objectName();
-        if (query != null && !getShortPath(userFolderPath, fullPath).contains(query)) {
-            return true;
-        }
-        return !fullPath.endsWith(PATH_SEPARATOR) || userFolderPath.equals(fullPath);
+    private FolderDto mapItem(Item item, String userFolderPath) {
+        String path = getShortPath(userFolderPath, item.objectName());
+        return new FolderDto(
+                PathHelper.extractName(path),
+                PathHelper.extractParentFolder(path));
+    }
+
+    private boolean isFile(String path) {
+        return !path.endsWith(PATH_SEPARATOR);
     }
 
     private String getShortPath(String userFolderPath, String fullPath) {
